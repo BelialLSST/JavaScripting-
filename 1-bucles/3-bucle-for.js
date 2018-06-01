1bcab958d6bf34407e23ad3c4f953dee8552d99a
BelialLSST <lastra.sergio.2tec.1011@gmail.com>
2018-06-01 21:21:47 +0200

    Bucle for 3 hecho + adicional

--- 1-bucles/3-bucle-for.js
+++ 1-bucles/3-bucle-for.js
@@ -1,4 +1,15 @@
 // ej 3
 // muestra por consola la tabla del 7;
 // Utilizad el bucle for
+for (i = 0; i < 12; i++){
+    console.log(inueva);
+    var inueva = 7 * i
+}
 
+//Muestra la tabla que le insertes mediane node
+//
+// var numer = process.argv[2];
+// for (i = 0; i < 12; i++){
+//     console.log(inueva);
+//     var inueva = numer * i
+// }
