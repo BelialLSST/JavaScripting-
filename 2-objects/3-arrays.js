88083e5480b1cc1bb5460a2798e633451b9eb8e9
BelialLSST <lastra.sergio.2tec.1011@gmail.com>
2018-06-04 19:46:19 +0200

    Arrays hecho

--- 2-objects/3-arrays.js
+++ 2-objects/3-arrays.js
@@ -5,7 +5,20 @@
 
 
 // Aquí tu código
-
+let arr = [
+    {
+        name:'pepito',
+        age: 25
+    },
+    {
+        name:'pepito',
+        age: 30
+    },
+    {
+        name:'Jackson',
+        age: 21
+    }
+]
 
 // Hasta aquí
 
@@ -46,33 +59,33 @@ if (arr[0].name === arr[1].name) {
     console.log('suspendes');
 }
 
-// TEST 6
+// TEST 7
 if (arr[1].name === 'pepito') {
     console.log('apruebas');
 } else {
     console.log('suspendes');
 }
 
-// TEST 7
+// TEST 8
 if (arr[1].age > arr[2].age) {
     console.log('apruebas');
 } else {
     console.log('suspendes');
 }
-// TEST 8
+// TEST 9
 if (arr[0].age === 25) {
     console.log('apruebas');
 } else {
     console.log('suspendes');
 }
-// TEST 9
+// TEST 10
 if (typeof arr[2].name === 'string') {
     console.log('apruebas');
 } else {
     console.log('suspendes');
 }
 
-// TEST 10
+// TEST 11
 if (arr[2].name[0] === 'J') {
     console.log('apruebas');
 } else {
